5468b4349130d22574b25af01b7266708a6401f3
Vira Buniak <virynka.b@gmail.com>
2022-06-10 11:58:05 +0300

    Add new model - Members. Add ViewSets, change urls format.

--- network_users/serializers.py
+++ network_users/serializers.py
@@ -3,13 +3,16 @@ from rest_framework import serializers
 from .models import Group, User
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'login', 'sex', 'birth_date', 'group')
-
-
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
-        fields = ('id', 'name', 'public')
+        fields = ('id', 'name', 'public',)
+
+
+class UserSerializer(serializers.ModelSerializer):
+#    group = serializers.PrimaryKeyRelatedField(queryset=Group.objects.all(), many=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'login', 'sex', 'birth_date', 'group')
+ #       read_only_fields = ['login']
